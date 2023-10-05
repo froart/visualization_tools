66e901cb21aa6468e2c309086a28a9a4c0b99ee4
froart <artemiusxyz@gmail.com>
2023-10-05 10:04:52 +0200

    Restructured code to forward declare classes in header file. Only done for SDL2pp headers yet.

--- include/vt_framebuffer.hpp
+++ include/vt_framebuffer.hpp
@@ -1,45 +1,18 @@
 #pragma once
-
-#include <iostream>
 #include <vector>
-#include <utility>
 #include <string>
-#include <SDL2/SDL.h>
-#include <SDL2pp/SDL2pp.hh>
 
-/*
-typedef uint8_t  Uint8;
-typedef uint32_t Uint32;
-enum SDL_WindowFlags;
-
-namespace SDL2pp {
-
-  using namespace std;
- 
-  class Window {
-    public:
-      Window( const string&, unsigned int, unsigned int, const unsigned int&, const unsigned int&, SDL_WindowFlags );
-  };
-  class Renderer
-  {
-    public:
-      Renderer( Window, int, Uint32 );
-      int SetDrawColor( Uint8, Uint8, Uint8, Uint8 );
-      int DrawPoint( int, int );
-      int Present();
-  };
-  class Exception
-  {
-    public:
-      string GetSDLFunction();
-      string GetSDLError();
-  };
+namespace SDL2pp 
+{
+  class Window;
+  class Renderer;
+  class Exception;
 }
-*/
 
 namespace vt 
 {
-  using namespace std;
+
+  // using namespace std;
 
   class FrameBuffer
   {
@@ -55,36 +28,20 @@ namespace vt
         public:
 
           SDL_Pixel() {};
-          SDL_Pixel( SDL2pp::Renderer* renderer, int rowIndex, int colIndex ) 
-          {
-            this->renderer = renderer;
-            this->rowIndex = rowIndex;
-            this->colIndex = colIndex;
-          }; 
-          SDL_Pixel( SDL_Pixel&& other ) noexcept : renderer( exchange( other.renderer, nullptr ) ),
-                                                    colIndex( exchange( other.colIndex, 0 ) ), 
-                                                    rowIndex( exchange( other.rowIndex, 0 ) ) {}; 
-          SDL_Pixel& operator= ( SDL_Pixel&& other ) noexcept 
-          {
-            if( this != &other ) 
-            {
-              renderer = exchange( other.renderer, nullptr );
-              rowIndex = exchange( other.rowIndex, 0 ); 
-              colIndex = exchange( other.colIndex, 0 ); 
-            }
-            return *this; 
-          }; 
-          SDL_Pixel& operator= ( initializer_list<int> );
+          SDL_Pixel( SDL2pp::Renderer*, unsigned int, unsigned int ); 
+          SDL_Pixel( SDL_Pixel&& ) noexcept;
+          SDL_Pixel& operator= ( SDL_Pixel&& ) noexcept; 
+          SDL_Pixel& operator= ( std::initializer_list<int> );
 
           SDL_Pixel( const SDL_Pixel& ) = delete;
-          SDL_Pixel& operator= ( SDL_Pixel& ) = delete;
+          SDL_Pixel& operator= ( const SDL_Pixel& ) = delete;
           ~SDL_Pixel();
         /* public */
       }; // class SDL_Pixel
     
       SDL2pp::Window*   window;
       SDL2pp::Renderer* renderer;
-      vector<SDL_Pixel> pixels;
+      std::vector<SDL_Pixel> pixels;
       float             aspectRatio;
       unsigned int      width;
       unsigned int      height;
@@ -94,39 +51,18 @@ namespace vt
     public:
 
       FrameBuffer() {};
-      FrameBuffer( const string, const unsigned int, const unsigned int );
-      FrameBuffer& operator= ( FrameBuffer&& other ) noexcept
-      {
-        if( this != &other ) 
-        { 
-          this->pixels       = move( other.pixels );           
-          this->aspectRatio  = exchange( other.aspectRatio,  0.0 );
-          this->width        = exchange( other.width,          0 );
-          this->height       = exchange( other.height,         0 );
-          this->pixelNumber  = exchange( other.pixelNumber,    0 );
-          this->size         = exchange( other.size,           0 );
-          this->window       = exchange( other.window,   nullptr );
-          this->renderer     = exchange( other.renderer, nullptr );
-        }
-        return *this;
-      }
+      FrameBuffer( const std::string, const unsigned int, const unsigned int );
+      FrameBuffer& operator= ( FrameBuffer&& ) noexcept;
 
-      unsigned int getWidth()    const { return this->width;        }
-      unsigned int getHeight()   const { return this->height;       }
-      unsigned int getPixelNum() const { return this->pixelNumber;  }
-      unsigned int getSize()     const { return this->size;         }
-      float getAspectRatio()     const { return this->aspectRatio;  }
+      unsigned int getWidth() const;
+      unsigned int getHeight() const;
+      unsigned int getPixelNum() const;
+      unsigned int getSize() const;
+      float getAspectRatio() const;
 
-      SDL_Pixel& operator() ( const unsigned int rowIndex, 
-                              const unsigned int colIndex ) 
-      {
-        return this->pixels[colIndex + this->width * rowIndex];
-      };
-      SDL_Pixel& operator[] ( unsigned int index ) 
-      { 
-        return this->pixels[index]; 
-      };
-      
+      SDL_Pixel& operator() ( const unsigned int, const unsigned int );
+      SDL_Pixel& operator[] ( unsigned int );
+     
       void update();
       bool requestedToExit();
 
--- src/vt_framebuffer.cpp
+++ src/vt_framebuffer.cpp
@@ -1,8 +1,39 @@
 #include "../include/vt_framebuffer.hpp"
+#include <SDL2/SDL.h>
+#include <SDL2pp/SDL2pp.hh>
+#include <utility>
+#include <iostream>
+#include <vector>
+#include <string>
 
 namespace vt {
 
   using namespace std;
+
+  FrameBuffer::SDL_Pixel::SDL_Pixel( SDL2pp::Renderer* renderer, unsigned int rowIndex, unsigned int colIndex ) 
+  {
+    this->renderer = renderer;
+    this->rowIndex = rowIndex;
+    this->colIndex = colIndex;
+  }; 
+
+  FrameBuffer::SDL_Pixel::SDL_Pixel( FrameBuffer::SDL_Pixel&& other ) noexcept 
+  {
+    this->renderer = exchange( other.renderer, nullptr );
+    this->colIndex = exchange( other.colIndex, 0 ); 
+    this->rowIndex = exchange( other.rowIndex, 0 ); 
+  }; 
+
+  FrameBuffer::SDL_Pixel& FrameBuffer::SDL_Pixel::operator= ( FrameBuffer::SDL_Pixel&& other ) noexcept 
+  {
+    if( this != &other ) 
+    {
+      renderer = exchange( other.renderer, nullptr );
+      rowIndex = exchange( other.rowIndex, 0 ); 
+      colIndex = exchange( other.colIndex, 0 ); 
+     }
+     return *this; 
+   }; 
 
   FrameBuffer::SDL_Pixel& FrameBuffer::SDL_Pixel::operator= ( initializer_list<int> colorValues ) {
     // TODO: check the size of passed initializer list at compile time, not runtime
@@ -57,6 +88,53 @@ namespace vt {
       for( int i = 0; i < width; ++i )
          ( *this )( j, i )= SDL_Pixel(this->renderer, j, i);
   }
+
+  FrameBuffer& FrameBuffer::operator= ( FrameBuffer&& other ) noexcept
+  {
+    if( this != &other ) 
+    { 
+      this->pixels       = move( other.pixels );           
+      this->aspectRatio  = exchange( other.aspectRatio,  0.0 );
+      this->width        = exchange( other.width,          0 );
+      this->height       = exchange( other.height,         0 );
+      this->pixelNumber  = exchange( other.pixelNumber,    0 );
+      this->size         = exchange( other.size,           0 );
+      this->window       = exchange( other.window,   nullptr );
+      this->renderer     = exchange( other.renderer, nullptr );
+    }
+    return *this;
+  }
+
+  unsigned int FrameBuffer::getWidth() const 
+  { 
+    return this->width;
+  }
+  unsigned int FrameBuffer::getHeight() const 
+  { 
+    return this->height;
+  }
+  unsigned int FrameBuffer::getPixelNum() const 
+  { 
+    return this->pixelNumber;
+  }
+  unsigned int FrameBuffer::getSize() const 
+  { 
+    return this->size;
+  }
+  float FrameBuffer::getAspectRatio() const 
+  { 
+    return this->aspectRatio;
+  }
+
+  FrameBuffer::SDL_Pixel& FrameBuffer::operator() ( const unsigned int rowIndex, 
+                                       const unsigned int colIndex ) 
+  {
+    return this->pixels[colIndex + this->width * rowIndex];
+  };
+  FrameBuffer::SDL_Pixel& FrameBuffer::operator[] ( unsigned int index ) 
+  { 
+    return this->pixels[index]; 
+  };
 
   void FrameBuffer::update()
   {
